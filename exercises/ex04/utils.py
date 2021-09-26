63be2723b2c532c7c1db4f87113e9eb40e10234e
Sam Moore <smoore64@live.unc.edu>
2021-09-26 18:19:12 -0400

    utils exercise 04 work

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,41 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730307980"
 
 
-# TODO: Implement your functions here.
+# all function
+
+
+def all(a: list[int], b: int) -> bool:
+    i: int = 0
+    while i < len(a):
+        if a[i] == b:
+            i += 1
+        else:
+            return False
+    return True
+
+
+# is_equal function
+
+
+def is_equal(a: list[int], b: list[int]) -> bool:
+    i = 0 
+    while i < len(a) or i < len(b):
+        if a[i] == b[i]:
+            i += 1
+        else:
+            return False
+    return True
+
+
+# max secion
+
+
+def max(input: list[int]) -> int:
+    big: int = max(input)
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    else:
+        print(big)
+    return big
